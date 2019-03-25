9bf858a29021c13c2f06ccd66403eda2cfa0ef60
omarperacha <omar.peracha@gmail.com>
2019-03-25 23:28:41 +0000

    all telelport

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -23,7 +23,6 @@ export default class HelloWorldSceneAR extends Component {
     // Set initial state here
     this.state = {
         appState: AppState.currentState,
-        section: 0,
         origin: [0,0,0]
     };
 
@@ -31,32 +30,36 @@ export default class HelloWorldSceneAR extends Component {
     // bind 'this' to functions
     this._onInitialized = this._onInitialized.bind(this);
       this._update = this._update.bind(this);
-      this._onTouchAlien = this._onTouchAlien.bind(this)
-      this._onTouchMetalLo = this._onTouchMetalLo.bind(this)
-      this._onTouchMetalHi = this._onTouchMetalHi.bind(this)
-      this._teleport = this._teleport.bind(this)
-      this._getPos = this._getPos.bind(this)
+      this._onTouchAlien = this._onTouchAlien.bind(this);
+      this._onTouchMetalLo = this._onTouchMetalLo.bind(this);
+      this._onTouchMetalHi = this._onTouchMetalHi.bind(this);
+      this._teleport1 = this._teleport1.bind(this);
+      this._teleport2 = this._teleport2.bind(this);
+      this._teleport3 = this._teleport3.bind(this);
+      this._teleport4 = this._teleport4.bind(this);
+      this._teleport5 = this._teleport5.bind(this);
+      this._getPos = this._getPos.bind(this);
   }
 
   render() {
     return (
             <ViroARScene onTrackingUpdated={this._onInitialized} onCameraTransformUpdate={this._update}>
             < // guitar
-            ViroBox position={this._getPos([0, 0, -1])} scale={[.3, .3, .3]} onClick={this._teleport}/>
+            ViroBox position={this._getPos([0, 0, -1])} scale={[.3, .3, .3]} onClick={this._teleport1}/>
             <ViroBox position={this._getPos([0.5, 0.3, -1])} scale={[.22, .22, .22]} rotation={[20, -30, 0]} materials={["black"]} onClick={this._onTouchMetalHi} />
             <ViroBox position={this._getPos([-0.5, 0.3, -1])} scale={[.22, .22, .22]} rotation={[20, 30, 0]} materials={["black"]} onClick={this._onTouchMetalLo} />
             < // pure
-            ViroPolyline position={this._getPos([-3, 0, 1])} points={[[0,-1,0], [0,1,0.15]]} thickness={0.2} />
-            <ViroPolyline position={this._getPos([-1.9, 0, 0])} points={[[0,-1,0], [-0.15,1,0.15]]} thickness={0.2} />
-            <ViroPolyline position={this._getPos([-4.1, 0, 0])} points={[[0,-1,0], [0.15,1,0.15]]} thickness={0.2} />
+            ViroPolyline position={this._getPos([-3, 0, 1])} points={[[0,-1,0], [0,1,0.15]]} thickness={0.2} onClick={this._teleport5}/>
+            <ViroPolyline position={this._getPos([-1.9, 0, 0])} points={[[0,-1,0], [-0.15,1,0.15]]} thickness={0.2} onClick={this._teleport5}/>
+            <ViroPolyline position={this._getPos([-4.1, 0, 0])} points={[[0,-1,0], [0.15,1,0.15]]} thickness={0.2} onClick={this._teleport5}/>
             < // bass
-            ViroPolyline position={this._getPos([-3.5, -1.3, -1.75])} points={[[-.5,0,-.5], [0,.5,0], [.5,0,-.5], [0,.5,-1], [-.5,0,-.5]]} thickness={0.1} />
+            ViroPolyline position={this._getPos([-3.5, -1.3, -1.75])} points={[[-.5,0,-.5], [0,.5,0], [.5,0,-.5], [0,.5,-1], [-.5,0,-.5]]} thickness={0.1} onClick={this._teleport4}/>
             < // alien
-            ViroSphere position={this._getPos([-1.5, -0.2, -3.5])} radius={.25} />
-            <ViroSphere position={this._getPos([-1.5, -0.2, -2.5])} radius={.25} />
+            ViroSphere position={this._getPos([-1.5, -0.2, -3.5])} radius={.25} onClick={this._teleport3}/>
+            <ViroSphere position={this._getPos([-1.5, -0.2, -2.5])} radius={.25} onClick={this._teleport3}/>
             <ViroSphere position={this._getPos([-2.3, 0.2, -3])} radius={.15} materials={["black"]} onClick={this._onTouchAlien} />
             < // conv
-            ViroBox position={this._getPos([0.5, 0, -3])} scale={[.3, .3, .3]} />
+            ViroBox position={this._getPos([0.8, 0, -3])} scale={[.3, .3, .3]} onClick={this._teleport2}/>
       </ViroARScene>
     );
   }
@@ -115,13 +118,44 @@ export default class HelloWorldSceneAR extends Component {
         return [x, y, z]
     }
     
-    _teleport(){
-        const newSec = this.state.section + 1
+    _teleport1(position, source){
+        const toLocation = [0,0,0]
         this.setState({
-                      section : newSec,
-                      origin : [0,0,2]
+                      origin : toLocation
                       });
-        this.conductor.teleport([0,0,2])
+        this.conductor.teleport(toLocation)
+    }
+    
+    _teleport2(position, source){
+        const toLocation = [-0.8,0,2]
+        this.setState({
+                      origin : toLocation
+                      });
+        this.conductor.teleport(toLocation)
+    }
+    
+    _teleport3(position, source){
+        const toLocation = [1,0,3]
+        this.setState({
+                      origin : toLocation
+                      });
+        this.conductor.teleport(toLocation)
+    }
+    
+    _teleport4(position, source){
+        const toLocation = [2.7,0,1.75]
+        this.setState({
+                      origin : toLocation
+                      });
+        this.conductor.teleport(toLocation)
+    }
+    
+    _teleport5(position, source){
+        const toLocation = [3,0,0]
+        this.setState({
+                      origin : toLocation
+                      });
+        this.conductor.teleport(toLocation)
     }
     
 }
